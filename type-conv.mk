adf5731392a09cf7f6bdd96177804c4d22c52470
Thomas Gazagnaire <thomas.gazagnaire@citrix.com>
2009-09-16 15:34:27 +0100

    [rpc-light] Download and install the type-conv package.

---
+++ type-conv.mk
@@ -0,0 +1,34 @@
+V=1.6.8
+
+NAME=type-conv-release-$(V)
+PACKAGE=release-$(V).tar.bz2
+URL=http://hg.ocaml.info/release/type-conv/archive/$(PACKAGE)
+
+SRC=$(OBJDIR)/$(NAME)
+
+$(DOWNLOADED):
+	echo $(SRC_DIR)
+	@mkdir -p $(SRC_DIR)
+	bash -c 'if [ ! -e $(SRC_DIR)/$(PACKAGE) ]; then if [ -e $(DISTFILES)/$(PACKAGE) ]; then cp $(DISTFILES)/$(PACKAGE) $(SRC_DIR); else wget $(URL) --output-document=$(SRC_DIR)/$(PACKAGE); fi; fi'
+	@touch $@
+
+$(EXTRACTED): $(DOWNLOADED)
+	cd $(OBJDIR) && tar -jxf $(SRC_DIR)/$(PACKAGE)
+	@touch $@
+
+$(CONFIGURED): $(PATCHED)
+	@touch $@
+
+$(BUILT): $(CONFIGURED)
+	$(MAKE) -C $(SRC)
+	@touch $@
+
+$(FAKED): $(BUILT)
+	$(MAKE) -C $(SRC) PREFIX=$(DESTDIR)$(PREFIX) install
+	@touch $@
+
+$(SOURCES):
+	echo ocaml gpl file $(DISTFILES)$(PACKAGE) > $@
+
+clean::
+	rm -rf $(SRC)
